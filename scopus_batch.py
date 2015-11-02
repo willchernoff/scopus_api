833783ec7fb43b09a73dff974e5b9950e2b9b744
Will Chernoff <willchernoff@gmail.com>
2015-11-02 17:11:09 -0600

    Transfered eids to a CSV file. Code now reads eids in from a CSV file.

--- scopus_batch.py
+++ scopus_batch.py
@@ -19,6 +19,8 @@ import scopus_api_key                    # Import Scopus API key
 
 from scrape_abstract_retrieval import update_dict_key, traverse_contents, article_scrape         # Import code scrape
 
+import csv
+
 import unicodecsv                        # Write to CSV, unicode
 
 
@@ -27,10 +29,15 @@ my_api_key = scopus_api_key.get_api_key()    # Scopus API key. Provides access t
 
     
 #### Add list of article ID codes
+with open('eid_list.csv', 'rU') as infile:
+    eid_list = [item for sublist in csv.reader(infile) for item in sublist]
+
+print 'List of unique eids: ', eid_list
+
 #eid_list = ['2-s2.0-84898664430', '2-s2.0-84912014389', '2-s2.0-84893613312', '2-s2.0-84903170614', '2-s2.0-84898919790', '2-s2.0-84880767928', '2-s2.0-84876309901', '2-s2.0-84920495370', '2-s2.0-84923284926', '2-s2.0-84923108032', '2-s2.0-84903640795', '2-s2.0-84905869743', '2-s2.0-84905859493', '2-s2.0-84905463440', '2-s2.0-84905460020', '2-s2.0-84902651495', '2-s2.0-84918784767', '2-s2.0-84899884487', '2-s2.0-84906736737', '2-s2.0-84926287456', '2-s2.0-84894454394', '2-s2.0-84922470444', '2-s2.0-84875020851']
 
 #eid_list = ['2-s2.0-84905859493']
-eid_list = ['2-s2.0-0000201308'] # Bridges and Steen
+#eid_list = ['2-s2.0-0000201308'] # Bridges and Steen
 #eid_list = ['2-s2.0-73449111286'] # TypeError: 'NoneType' object is not iterable
 
 print len(eid_list) # Print number of article IDs # 23
